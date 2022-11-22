886dc3f60bca837a44d8883219ec6b9b6597c5d2
Antonia Jaimeson-Shirley <112460380+antoniajs@users.noreply.github.com>
2022-11-22 19:22:54 +0000

    added new template files and more code to main

--- main.py
+++ main.py
@@ -1,34 +1,70 @@
-from flask import Flask, request, jsonify 
-from collections import defaultdict
-import json
+from flask import Flask, render_template, request
+from replit import db
+app = Flask('app')
 
-app = Flask(__name__)
-app.data = defaultdict(int)
-@app.route("/", methods=['GET'])
-@app.route("/home", methods=['GET'])
+db["number1"] = "Dr.A"
+db["score1"] = 20
+db["number2"] = "Danielle"
+db["score2"] = 18
+db["number3"] = "Shemaya"
+db["score3"] = 17
+db["number4"] = "Antonia"
+db["score4"] = 10
+db["number5"] = "Brandon"
+db["score5"] = 3
+
+@app.route('/')
+def homepage():
+  number1 = db["number1"] + ": " + str(db["score1"])
+  number2 = db["number2"] + ": " + str(db["score2"])
+  number3 = db["number3"] + ": " + str(db["score3"])
+  number4 = db["number4"] + ": " + str(db["score4"])
+  number5 = db["number5"] + ": " + str(db["score5"])
+  return render_template('leaderboard.html', number1=number1, number2=number2, number3=number3, number4=number4, number5=number5)
 
 
-@app.route("/get_info", methods=['GET'])
-def get_info():
-  data = request.data
-  data = json.loads(data)
-  
-  users = [("username", 5), ("someone", 15), ('n00b', 0)]
-  
-  # Sort user array
-  users.sort(key=lambda a: a[1], reverse=True)
+@app.route("/leaderboard", methods=["POST", "GET"])
+def leaderboard():
+  username = request.form["name_input"]
+  score = request.form["score_input"]
+  if score > db["score1"]:
+    db["number5"] = db["number4"]
+    db["score5"] = db["score4"]
+    db["number4"] = db["number3"]
+    db["score4"] = db["score3"]
+    db["number3"] = db["number2"]
+    db["score3"] = db["score2"]
+    db["number1"] = username
+    db["score1"] = score
+  elif score > db["score2"]:
+    db["number5"] = db["number4"]
+    db["score5"] = db["score4"]
+    db["number4"] = db["number3"]
+    db["score4"] = db["score3"]
+    db["number3"] = db["number2"]
+    db["score3"] = db["score2"]
+    db["number2"] = username
+    db["score2"] = score
+  elif score > db["score3"]:
+    db["number5"] = db["number4"]
+    db["score5"] = db["score4"]
+    db["number4"] = db["number3"]
+    db["score4"] = db["score3"]
+    db["number3"] = username
+    db["score3"] = score
+  elif score > db["score4"]:
+    db["number5"] = db["number4"]
+    db["score5"] = db["score4"]
+    db["number4"] = username
+    db["score4"] = score
+  elif score > db["score5"]:
+    db["number5"] = username
+    db["score5"] = score
+  number1 = db["number1"] + ": " + str(db["score1"])
+  number2 = db["number2"] + ": " + str(db["score2"])
+  number3 = db["number3"] + ": " + str(db["score3"])
+  number4 = db["number4"] + ": " + str(db["score4"])
+  number5 = db["number5"] + ": " + str(db["score5"])
+  return render_template('leaderboard.html', number1=number1, number2=number2, number3=number3, number4=number4, number5=number5)
 
-  # Format the users
-  leaderboard = map(lambda user: user[0] + "| " + str(user[1]), users)
-  
-  # Output
-  print("\n".join(leaderboard))
-  return jsonify({'GET' : ' works'})
-
-@app.route("/test_post", methods=['POST'])
-def test_post():
-  return jsonify({'status': 'post works', 'your_data': json.loads(request.data)})
-
-  
-
-app.run(host='0.0.0.0')
+app.run(host='0.0.0.0', port=81)
